2ae5f41481a08136ed893b88eb3cf9310539cee5
jonathanbataire <jonathanbataire@gmail.com>
2020-11-24 15:52:24 +0300

    packs clean up

--- packs/buildpacks.sh
+++ packs/buildpacks.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 mkdir -p store/apks
 i=1
-while [ $i -le 1 ]
+while [ $i -le 10 ]
 do
     echo "Building pack $i"
     export PACK_NUMBER=$i
     ../gradlew -PpackNumber=$i build
-    cp build/outputs/apk/debug/packs-debug.apk store/apks/pack$i.apk
+    cp build/outputs/apk/release/packs-release.apk store/apks/pack$i.apk
     i=`expr $i + 1`
 done
-
